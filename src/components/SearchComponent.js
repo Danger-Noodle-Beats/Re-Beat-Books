f232b0ee8e920ba21e4fcb0b6d962f8efa8474e8
chunhz <chz062009@gmail.com>
2022-09-06 23:04:19 -0400

    done with adding comments and updated some command for GitHub instructions

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,47 +1,67 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import "../stylesheet/styles.css";
-import logo from "../../assets/logo.png"
+import '../stylesheet/styles.css';
+import logo from '../../assets/logo.png';
 
 function SearchComponent() {
-    const [bookName, setBookName] = useState('');
-    const [author, setAuthor] = useState('');
+	//initating react hooks
+	const [bookName, setBookName] = useState('');
+	const [author, setAuthor] = useState('');
 
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    const handleBookName = e => {
-        setBookName(e.target.value);
-    };
-    const handleAuthor = e => {
-        setAuthor(e.target.value);
-    };
+	const handleBookName = (e) => {
+		setBookName(e.target.value);
+	};
+	const handleAuthor = (e) => {
+		setAuthor(e.target.value);
+	};
 
-    const handleSubmit = () => {
-        if (!bookName || !author) {
-            alert("Please enter both fields.");
-            return;
-        }
-        navigate("/loading", {state: { book: bookName, author: author}});
-    }
+	const handleSubmit = () => {
+		if (!bookName || !author) {
+			alert('Please enter both fields.');
+			return;
+		}
+		// redirecting to loading component
+		navigate('/loading', { state: { book: bookName, author: author } });
+	};
 
-    return (
-        <div>
-            <div className="form">
-                <div className="header">
-                    <img src={logo} width={'50%'} />
-                </div>
-                <b><label className="label">Book Name</label></b>
-                <input type='text' value={bookName} onChange={handleBookName} className="userInput" />
-                <br />
-                <b><label className="label"> Author </label></b>
-                <input type='text' value={author} onChange={handleAuthor} className="userInput" />
-                <br />
-                <button onClick={handleSubmit} className="submitButton">Submit</button>
-            </div >
-        </div >
-    )
+	return (
+		<div>
+			<div className="form">
+				<div className="header">
+					<img src={logo} width={'50%'} />
+				</div>
+				<b>
+					{/* creating input file for book names */}
+					<label className="label">Book Name</label>
+				</b>
+
+				<input
+					type="text"
+					value={bookName}
+					onChange={handleBookName}
+					className="userInput"
+				/>
+				<br />
+				<b>
+					{/* creating input file for author names */}
+					<label className="label"> Author </label>
+				</b>
+
+				<input
+					type="text"
+					value={author}
+					onChange={handleAuthor}
+					className="userInput"
+				/>
+				<br />
+				<button onClick={handleSubmit} className="submitButton">
+					Submit
+				</button>
+			</div>
+		</div>
+	);
 }
-
-
 
 export default SearchComponent;
