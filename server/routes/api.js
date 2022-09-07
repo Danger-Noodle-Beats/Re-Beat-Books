6f6013e3a546bc8785cedef991f90b4bde5c05eb
Matthew Garza <mattg614@gmail.com>
2022-09-07 12:16:35 -0400

    Add comments for api file

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   console.log('success in router.get');
+  // create: recent playlist: book name, author, song name
+  // update: favirote
+  // delete the created playlist
   res.status(200).json('Success');
 });
 
